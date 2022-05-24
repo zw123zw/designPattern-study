d2a72d090d115d6183dd7ef3aaab2574620cbc85
zw123zw <zw18674069215@163.com>
2022-05-24 08:28:03 +0800

    工厂模式学习

--- js/pattern1.js
+++ js/pattern1.js
@@ -1,36 +1,61 @@
 // 简单工厂模式
-let LoginAlert = function(text){
-    this.content = text
+let LoginAlert = function (text) {
+  this.content = text
 }
-LoginAlert.prototype.show = function(){
-    alert(this.content)
+LoginAlert.prototype.show = function () {
+  alert(this.content)
 }
-let LoginConfirm = function(text){
-    this.content = text
+let LoginConfirm = function (text) {
+  this.content = text
 }
-LoginConfirm.prototype.show = function(){
-    console.log(this.content)
+LoginConfirm.prototype.show = function () {
+  console.log(this.content)
 }
-let LoginPrompt = function(text){
-    this.content = text
+let LoginPrompt = function (text) {
+  this.content = text
 }
-LoginPrompt.prototype.show = function(){
-    console.log(this.content)
+LoginPrompt.prototype.show = function () {
+  console.log(this.content)
 }
 let la = new LoginPrompt('欢迎回来')
 la.show()
 // 工厂
-function sport(type, text){
-    switch(type){
-        case 'alert':
-            return new LoginAlert(text)
-        case 'confirm':
-            return new LoginConfirm(text)
-        case 'prompt':
-            return new LoginPrompt(text)
-        default:
-            break
-    }
+function sport(type, text) {
+  switch (type) {
+    case 'alert':
+      return new LoginAlert(text)
+    case 'confirm':
+      return new LoginConfirm(text)
+    case 'prompt':
+      return new LoginPrompt(text)
+    default:
+      break
+  }
 }
 let ls = sport('confirm', '注册成功')
 ls.show()
+function createPop(type, text) {
+  let o = new Object()
+  o.content = text
+  o.type = type
+  o.show = function () {}
+  if (o.type === 'alert') {
+  }
+  if (o.type === 'confirm') {
+  }
+  if (o.type === 'prompt') {
+  }
+}
+// 安全工厂方法
+let Factory = function (type, content) {
+  if (this instanceof Factory) {
+    return this[type][content]
+  } else {
+    return new Factory(type, content)
+  }
+}
+Factory.prototype = {
+  Jave: function () {},
+  Php: function () {},
+  Jascript: function () {},
+}
